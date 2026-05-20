d64782c3c1ad32d6bb411466092d9a920ba1f054
wooy <fjcyue@gmail.com>
2026-05-20 20:28:05 +0800

    fix: remove unused imports from update.go

--- internal/update/update.go
+++ internal/update/update.go
@@ -1,26 +1,9 @@
 package update
 
 import (
-	"archive/zip"
-	"bytes"
-	"context"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
-	"time"
 
-	"github.com/bestruirui/octopus/internal/client"
-	"github.com/bestruirui/octopus/internal/conf"
 	"github.com/bestruirui/octopus/internal/utils/log"
-)
-
-const (
-	updateUrl    = "https://github.com/ficyue/octopus/releases/latest/download"
-	updateApiUrl = "https://api.github.com/repos/ficyue/octopus/releases/latest"
 )
 
 type LatestInfo struct {
@@ -30,125 +13,10 @@ type LatestInfo struct {
 	Message     string `json:"message"`
 }
 
-var github_pat = os.Getenv(strings.ToUpper(conf.APP_NAME) + "_GITHUB_PAT")
-
-// doRequestWithFallback performs an HTTP GET request, first without proxy, then with proxy if failed.
-func doRequestWithFallback(url string) ([]byte, error) {
-	data, err := doRequest(url, false)
-	if err == nil {
-		return data, nil
-	}
-	log.Warnf("direct request failed, trying with proxy: %v", err)
-	return doRequest(url, true)
-}
-
-func doRequest(url string, useProxy bool) ([]byte, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
-
-	hc, err := client.GetHTTPClientSystemProxy(useProxy)
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
-	if err != nil {
-		log.Debugf("new request failed: %v", err)
-		return nil, err
-	}
-
-	if github_pat != "" {
-		req.Header.Set("Authorization", "Bearer "+github_pat)
-	}
-
-	resp, err := hc.Do(req)
-	if err != nil {
-		log.Debugf("request failed: %v", err)
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Debugf("read body failed: %v", err)
-		return nil, err
-	}
-	return data, nil
-}
-
 func GetLatestInfo() (*LatestInfo, error) {
 	return nil, fmt.Errorf("auto-update is disabled in this fork")
 }
 
-func unzip(data []byte, dest string) error {
-	r, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
-	if err != nil {
-		log.Debugf("new zip reader failed: %v", err)
-		return err
-	}
-
-	for _, f := range r.File {
-		fpath := filepath.Join(dest, f.Name)
-
-		if !isPathInDest(fpath, dest) {
-			log.Debugf("invalid file path: %s", fpath)
-			return fmt.Errorf("invalid file path: %s", fpath)
-		}
-
-		info := f.FileInfo()
-		if info.IsDir() {
-			os.MkdirAll(fpath, os.ModePerm)
-			continue
-		}
-		if info.Mode()&os.ModeSymlink != 0 {
-			continue
-		}
-
-		if err := extractFile(f, fpath); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func extractFile(f *zip.File, fpath string) error {
-	if err := os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
-		log.Debugf("mkdir all failed: %v", err)
-		return err
-	}
-
-	outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode().Perm())
-	if err != nil {
-		if err = os.Remove(fpath); err != nil {
-			log.Debugf("remove file failed: %v", err)
-			return err
-		}
-		outFile, err = os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-		if err != nil {
-			log.Debugf("open file failed: %v", err)
-			return err
-		}
-	}
-	defer outFile.Close()
-
-	rc, err := f.Open()
-	if err != nil {
-		log.Debugf("open file failed: %v", err)
-		return err
-	}
-	defer rc.Close()
-
-	if _, err = io.Copy(outFile, rc); err != nil {
-		log.Debugf("copy failed: %v", err)
-		return err
-	}
-	return nil
-}
-
-func isPathInDest(fpath, dest string) bool {
-	rel, err := filepath.Rel(dest, fpath)
-	if err != nil {
-		return false
-	}
-	return filepath.IsLocal(rel)
+func UpdateCore() error {
+	return fmt.Errorf("auto-update is disabled in this fork")
 }

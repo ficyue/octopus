91a7fe9de550a748b9991bd7a43a2c653d24b991
wooy <fjcyue@gmail.com>
2026-05-20 21:04:52 +0800

    fix: add missing inbound import in type.go

--- internal/relay/type.go
+++ internal/relay/type.go
@@ -8,6 +8,7 @@ import (
 	"github.com/bestruirui/octopus/internal/conf"
 	dbmodel "github.com/bestruirui/octopus/internal/model"
 	"github.com/bestruirui/octopus/internal/relay/balancer"
+	"github.com/bestruirui/octopus/internal/transformer/inbound"
 	"github.com/bestruirui/octopus/internal/transformer/model"
 	"github.com/gin-gonic/gin"
 )
